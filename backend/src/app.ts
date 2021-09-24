d8565c05c05167e96bc106658cc15958e9641885
ironore15 <37268740+ironore15@users.noreply.github.com>
2021-09-24 06:54:56 +0000

    Configure basic typescript and express settings

---
+++ backend/src/app.ts
@@ -0,0 +1,11 @@
+'use strict';
+import express from 'express'
+const app = express();
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(8080, () => {
+  console.log('Example app listening on port 8080!');
+});
