0a0dd264868a724f079c8ffcf791f7a83aeb3e92
HyeongChang Lee <hc9904@hanyang.ac.kr>
2021-10-08 09:45:31 +0900

    Update Validation

---
+++ frontend/src/shared/components/Interceptor.tsx
@@ -0,0 +1,59 @@
+import axios, {
+  AxiosResponse,
+  AxiosError,
+  AxiosInstance,
+  AxiosRequestConfig,
+} from "axios";
+
+import CONFIG from "config";
+
+const baseURL = CONFIG.API_SERVER;
+
+class Interceptor {
+  private readonly instance: AxiosInstance;
+
+  constructor() {
+    this.instance = axios.create({
+      baseURL,
+    });
+
+    this.instance = this.setupInterceptorsTo(this.instance);
+  }
+
+  getInstance() {
+    return this.instance;
+  }
+
+  private onRequest = (config: AxiosRequestConfig): AxiosRequestConfig => {
+    console.info(`[request] [${JSON.stringify(config)}]`);
+    return config;
+  };
+
+  private onRequestError = (error: AxiosError): Promise<AxiosError> => {
+    console.error(`[request error] [${JSON.stringify(error)}]`);
+    return Promise.reject(error);
+  };
+
+  private onResponse = (response: AxiosResponse): AxiosResponse => {
+    console.info(`[response] [${JSON.stringify(response)}]`);
+    return response;
+  };
+
+  private onResponseError = (error: AxiosError): Promise<AxiosError> => {
+    console.error(`[response error] [${JSON.stringify(error)}]`);
+    return Promise.reject(error);
+  };
+
+  private setupInterceptorsTo(axiosInstance: AxiosInstance): AxiosInstance {
+    axiosInstance.interceptors.request.use(this.onRequest, this.onRequestError);
+    axiosInstance.interceptors.response.use(
+      this.onResponse,
+      this.onResponseError
+    );
+    return axiosInstance;
+  }
+
+  hello() {}
+}
+
+export default new Interceptor();
