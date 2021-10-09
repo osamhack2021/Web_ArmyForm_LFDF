5bce821d73567aa36ba4e872abbd8d84ee8d3d05
hyunwoo0081 <hyunwoo0081@gmail.com>
2021-10-09 14:39:59 +0000

    Responsive Design

--- frontend/src/container/Survey/index.tsx
+++ frontend/src/container/Survey/index.tsx
@@ -9,6 +9,8 @@ import Slider from "container/component/Slider";
 
 import "style/Survey.scss";
 
+let removeResize:Function;
+
 class Survey extends Component {
   state = {
     page1_count: 7,//Math.round(data1.length / column_count);
@@ -16,27 +18,45 @@ class Survey extends Component {
     page3_count: 3,
     
     page1_pointer: 0,
-    page2_pointer: 1,
+    page2_pointer: 0,
     page3_pointer: 0,
+
+    window_width: 0
+  }
+
+  componentWillMount(){
+    this.setState({window_width: window.innerWidth});
+    const handleWidth = () => {
+      this.setState({window_width: window.innerWidth});
+    };
+    window.addEventListener('resize', handleWidth);
+
+    removeResize = () => {
+      window.removeEventListener('resize', handleWidth);
+    }
+  }
+
+  componentWillUnmount(){
+    removeResize();
   }
 
   render(){
     //Survey Data
     const data1 = [
       {
-        type:4,
+        type:1,
         name:"전 군 성폭력 예방 설문조사",
         target:"전 군",
         deadline:1
       },
       {
-        type:5,
+        type:2,
         name:"사이버네트워크작전센터 부대정밀진단 설문조사",
         target:"사이버네트워크작전센터 전 장병",
         deadline:3
       },
       {
-        type:6,
+        type:3,
         name:"부대종합전투력측정 참가자 모집",
         target:"사이버네트워크작전센터 전 장병",
         deadline:5
@@ -47,19 +67,43 @@ class Survey extends Component {
         target:"10급양대 급양대상 전 장병",
         deadline:7
       },
+      {
+        type:5,
+        name:"9월 급양 선호도 설문조사",
+        target:"10급양대 급양대상 전 장병",
+        deadline:9
+      },
+      {
+        type:6,
+        name:"9월 급양 선호도 설문조사",
+        target:"10급양대 급양대상 전 장병",
+        deadline:11
+      },
+      {
+        type:0,
+        name:"9월 급양 선호도 설문조사",
+        target:"10급양대 급양대상 전 장병",
+        deadline:13
+      },
+      {
+        type:4,
+        name:"9월 급양 선호도 설문조사",
+        target:"10급양대 급양대상 전 장병",
+        deadline:15
+      },
     ];
 
-    let data2 = [{name:"1"},{name:"2"},{name:"3"},{name:"4"},{name:"5"},{name:"6"},{name:"7"}];
-    let data3 = [{name:"1"},{name:"2"},{name:"3"},{name:"4"},{name:"5"},{name:"6"},{name:"7"}];
+    let data2 = data1;
+    let data3 = data1;
 
-    //window 크기에 따라 Card 개수 다르게 하고 싶습니다.
-    const cardWidth = 257;
-    let column_count = Math.floor(innerWidth/cardWidth - 1);
+    const CARD_WIDTH = 227;
+    const availableWidth = (this.state.window_width > 1820 ? 1820 : this.state.window_width) - 180;
+    let column_count = Math.floor(availableWidth/CARD_WIDTH);
     if(column_count <= 0) column_count = 1;
 
     let page1_data = data1.slice(this.state.page1_pointer*column_count, (this.state.page1_pointer+1)*column_count);
     let page2_data = data2.slice(this.state.page2_pointer*column_count, (this.state.page2_pointer+1)*column_count);
-    let page3_data = data1.slice(this.state.page3_pointer*column_count, (this.state.page3_pointer+1)*column_count);
+    let page3_data = data3.slice(this.state.page3_pointer*column_count, (this.state.page3_pointer+1)*column_count);
 
     return (
       <>
@@ -86,13 +130,11 @@ class Survey extends Component {
                 <img src={LeftArrow} alt="<" />
               </button>
               <div className="card_list">
-                <SurveyCard type="0" name="전군 성폭력 예방 설문조사" target="전 군" deadline="0"/>
-                <SurveyCard type="1" name="전군 성폭력 예방 설문조사" target="전 군" deadline="5"/>
-                <SurveyCard type="2" name="전군 성폭력 예방 설문조사" target="전 군" deadline="7"/>
-                <SurveyCard type="3" name="전군 성폭력 예방 설문조사" target="전 군" deadline="15"/>
-                <SurveyCard type="4" name="전군 성폭력 예방 설문조사" target="전 군" deadline="0"/>
-                <SurveyCard type="5" name="전군 성폭력 예방 설문조사" target="전 군" deadline="2"/>
-                <SurveyCard type="7" name="전군 성폭력 예방 설문조사" target="전 군" deadline="0"/>
+                { 
+                  page1_data.map( (item) => {
+                    return <SurveyCard type={item.type} name={item.name} target={item.target} deadline={item.deadline}/>
+                  })
+                }
               </div>
               <button className="flat">
                 <img src={RightArrow} alt=">" />
@@ -110,9 +152,11 @@ class Survey extends Component {
                 <img src={LeftArrow} alt="<" />
               </button>
               <div className="card_list">
-                <SurveyCard type="0" name="전군 성폭력 예방 설문조사" target="전 군" deadline="0"/>
-                <SurveyCard type="0" name="전군 성폭력 예방 설문조사" target="전 군" deadline="0"/>
-                <SurveyCard type="0" name="전군 성폭력 예방 설문조사" target="전 군" deadline="0"/>
+                { 
+                  page2_data.map( (item) => {
+                    return <SurveyCard type="0" name={item.name} target={item.target} deadline={item.deadline}/>
+                  })
+                }
               </div>
               <button className="flat">
                 <img src={RightArrow} alt=">" />
@@ -131,8 +175,8 @@ class Survey extends Component {
               </button>
               <div className="card_list">
                 { 
-                  page3_data.map( (item, index) => {
-                    return <SurveyCard name={item.name} target={item.target} deadline={item.deadline}/>
+                  page3_data.map( (item) => {
+                    return <SurveyCard type="4" name={item.name} target={item.target} deadline={item.deadline}/>
                   })
                 }
               </div>
@@ -143,7 +187,7 @@ class Survey extends Component {
             <Slider current={this.state.page3_pointer} length={this.state.page3_count} />
           </div>
         </div>
-
+        
         <footer>
           <div>footer</div>
         </footer>
