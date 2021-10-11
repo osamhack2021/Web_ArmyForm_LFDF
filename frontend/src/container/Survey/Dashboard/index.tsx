353b833bcb833fde14733d92a05b4c74a4293604
hyunwoo0081 <hyunwoo0081@gmail.com>
2021-10-11 11:21:52 +0000

    Update Survey Page

--- frontend/src/container/Survey/Dashboard/index.tsx
+++ frontend/src/container/Survey/Dashboard/index.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import move from "shared/components/move";
 
 import LeftArrow from "static/left-arrow.png";
 import RightArrow from "static/right-arrow.png";
@@ -9,16 +12,49 @@ import SurveySlider from "container/component/SurveySlider";
 import "style/Survey/Dashboard.scss";
 
 const Dashboard = () => {
+  const history = useHistory();
+  //설문조사 데이터
+  const SurveyData = [
+    {
+      title:"Test Form",
+      contents:"테스트 데이터입니다."
+    },
+    {},
+    {},
+    {},
+    {},
+    {},
+    {}
+  ];
+
+  const SurveyPages = SurveyData.length;
+  const [ SurveyCursor, setCursor ] = useState(0);
+
+  function updateCursor(cursor: number){
+    if(0 <= cursor && cursor < SurveyPages){
+      window.scrollTo( 0, 0 );
+      setCursor(cursor);
+    }
+  }
+  
   return (
     <>
       <Nav title="FormName">
         <button className="flat">미리보기</button>
         <button className="flat">저장</button>
-        <button className="flat">나가기</button>
+        <button
+          className="flat"
+          onClick={ () => move(history, "/Survey") }
+        >
+          나가기
+        </button>
       </Nav>
 
       <div className="spread_row background_green">
-        <button className="flat">
+        <button
+          className="flat"
+          onClick={ () => updateCursor(SurveyCursor-1) }
+        >
           <img src={LeftArrow} alt="<" />
         </button>
         <div>
@@ -72,10 +108,17 @@ const Dashboard = () => {
               publishing software like Aldus PageMaker including versions of
               Lorem Ipsum.
             </p>
-            <SurveySlider current={0} length={5} />
+            <SurveySlider
+              current={ SurveyCursor }
+              length={ SurveyPages }
+              moveFunc={ (index) => updateCursor(index) }
+            />
           </div>
         </div>
-        <button className="flat">
+        <button
+          className="flat"
+          onClick={ () => updateCursor(SurveyCursor+1) }
+        >
           <img src={RightArrow} alt=">" />
         </button>
       </div>
