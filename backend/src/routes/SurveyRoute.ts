6e656b631ac49bcd662aa69e497ad746c31d73c8
tshong3 <48014624+tshong3@users.noreply.github.com>
2021-10-12 05:27:17 +0000

    add checkToken method in SurveyRoute

--- backend/src/routes/SurveyRoute.ts
+++ backend/src/routes/SurveyRoute.ts
@@ -5,17 +5,19 @@
 import express, { Request, Response, NextFunction } from 'express';
 import asyncHandler from 'express-async-handler';
 import SurveyController from '../controllers/SurveyController';
+import AuthMiddleware from '../middlewares/AuthMiddleware';
 
 const router = express.Router();
+const checkToken = AuthMiddleware.checkToken;
 
 router.get('/', SurveyController.index);
 
 // TODO: May consider divide file to Survey Management and Survey View.
 // TODO: Should add AuthMiddleware (check token etc...)
-router.post('/', asyncHandler(SurveyController.create));
-router.delete('/', asyncHandler(SurveyController.remove));
+router.post('/', [checkToken], asyncHandler(SurveyController.create));
+router.delete('/', [checkToken], asyncHandler(SurveyController.remove));
 
-router.get('/results', asyncHandler(SurveyController.results));
+router.get('/results', [checkToken], asyncHandler(SurveyController.results));
 
 router.get('*', (req: Request, res: Response, next: NextFunction) => {
   /* TODO: error handle */
