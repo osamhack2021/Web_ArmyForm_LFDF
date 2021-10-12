15edf4c5b12525b2a56187b6a8ccc19ba8ea3cb8
tshong3 <48014624+tshong3@users.noreply.github.com>
2021-10-12 05:26:32 +0000

    add check JWT Token method

--- backend/src/middlewares/AuthMiddleware.ts
+++ backend/src/middlewares/AuthMiddleware.ts
@@ -1,14 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
 import User from '../models/UserModel';
+import AuthConfig from '../configs/AuthConfig';
 
 class AuthMiddleware {
   public static index(req: Request, res: Response, next: NextFunction): any {
     return res.json({
-      msg: 'this is checkDuplicate test'
+      msg: 'this is checkDuplicate test or check Token'
     });
   }
 
-  public static async checkDuplicateUsernameOrSerial(req: Request, res: Response, next: NextFunction): Promise<any> {
+  public static async checkDuplicateUseridOrSerial(req: Request, res: Response, next: NextFunction): Promise<any> {
     User.findOne({
       where: {
         userid: req.body.userid
@@ -33,6 +35,32 @@ class AuthMiddleware {
           return;
         }
       })
+    })
+  }
+
+  public static async checkToken(req: any, res: Response, next: NextFunction): Promise<any> {
+    let token: any = req.headers["x-access-token"];
+
+    if (!token) {
+      return res.status(403).send({
+        message: "No token provided!"
+      })
+    }
+
+    jwt.verify(token, AuthConfig.SECRET, function (err: any, decoded: any) {
+      if (err) {
+        return res.status(401).send({
+          message: "Unauthorized!"
+        });
+      }
+      req.userId = decoded.id;
+      next();
+    })
+  }
+
+  // 유저 구분하기
+  public static async cadre(req: any, res: Response, next: NextFunction) {
+    User.findByPk(req.userId).then(user => {
     })
   }
 }
