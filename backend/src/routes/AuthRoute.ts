f475a9c482852b8b549ce7edd30cd399cd8abe5a
tshong3 <48014624+tshong3@users.noreply.github.com>
2021-10-15 11:39:25 +0000

    Change method name /signin -> /login

--- backend/src/routes/AuthRoute.ts
+++ backend/src/routes/AuthRoute.ts
@@ -12,7 +12,7 @@ const router = express.Router();
 router.get('/', AuthController.index);
 
 // TODO: Should add AuthMiddleware (check duplicate userid etc...)
-router.post('/signin', asyncHandler(AuthController.signin));
+router.post('/login', asyncHandler(AuthController.signin));
 router.post('/signup', [AuthMiddleware.checkDuplicateUseridOrSerial], asyncHandler(AuthController.signup));
 
 router.get('*', (req: Request, res: Response, next: NextFunction) => {
